192539c3dc08aaa5216db5a92bc6801bfd1c1bf4
WilliamWu <wuwei94@yeah.net>
2023-09-20 02:34:40 +0800

    add：Flutter GetX

---
+++ basic/basic_flutter/lib/get/my_get.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MyGetX extends StatelessWidget {
+  const MyGetX({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //GetMaterialApp 只是对 MaterialApp 封装了一层，它的子组件是默认的 MaterialApp。
+    //
+    // 如果你只用 Get 来进行状态管理或依赖管理，就没有必要使用GetMaterialApp。
+    // GetMaterialApp 对于路由、snackBar、国际化、bottomSheet、对话框以及与路由相关的高级 apis 和没有上下文（context）的情况下是必要的。
+    return GetMaterialApp(home: GetXRoute());
+  }
+}
+
+class Controller extends GetxController {
+  var count = 0.obs; // 可观察
+
+  void increment() {
+    count++;
+  }
+}
+
+class GetXRoute extends StatelessWidget {
+  GetXRoute({super.key});
+
+  final controller = Get.put(Controller());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("counter")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GetX<Controller>(
+                builder: (_) => Text(
+                      'clicks: ${controller.count}',
+                    )),
+            ElevatedButton(
+              child: const Text('Next Route'),
+              onPressed: () {
+                Get.to(Second());
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.increment,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Second extends StatelessWidget {
+  Second({super.key});
+
+  final Controller ctrl = Get.find();
+
+  @override
+  Widget build(context) {
+    return Scaffold(body: Center(child: Text("${ctrl.count}")));
+  }
+}
--- modules/module_flutter/src/main/java/com/example/william/my/module/flutter/activity/FlutterMainActivity.kt
+++ modules/module_flutter/src/main/java/com/example/william/my/module/flutter/activity/FlutterMainActivity.kt
@@ -46,6 +46,7 @@ class FlutterMainActivity : BasicRecyclerActivity() {
             "MySharedPreferences",
             "MyHttp",
             "MyIsolate",
+            "MyGetX",
         )
     }
 
